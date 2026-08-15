f17447012c5d0c58311f58b40b2bd4584027521e
cmtimran <imran527166@gmail.com>
2026-08-15 03:06:19 +0200

    fix: change default payment currency to USD

--- src/app/checkout/simplepay-pay/page.tsx
+++ src/app/checkout/simplepay-pay/page.tsx
@@ -26,7 +26,7 @@ function SimplePayContent() {
     fetch('/api/simplepay/start', {
       method:  'POST',
       headers: { 'Content-Type': 'application/json' },
-      body:    JSON.stringify({ productName, planTier, price, currency: 'HUF' }),
+      body:    JSON.stringify({ productName, planTier, price, currency: 'USD' }),
     })
       .then((res) => res.json())
       .then((data) => {
